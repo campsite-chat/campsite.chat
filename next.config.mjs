44946be41b005fbe9e3ae349056b309abea114c7
Ty Mason <hi@tygr.dev>
2024-08-23 16:39:53 +0100

    feat: add redirects

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/github",
+				destination: "https://github.com/campsite-chat",
+				permanent: true,
+			},
+			{
+				source: "/twitter",
+				destination: "https://twitter.com/campsitechat",
+				permanent: true,
+			},
+			{
+				source: "/instagram",
+				destination: "https://instagram.com/campsitechat",
+				permanent: true,
+			},
+		];
+	},
+};
 
 export default nextConfig
